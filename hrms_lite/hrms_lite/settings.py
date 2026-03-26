97f680a71c9d9e12566a32f3c6a6cfd2bee65309
Tejas Cargo India Limited <tcil@Mac.lan>
2026-03-26 14:54:03 +0530

    new update

--- hrms_lite/hrms_lite/settings.py
+++ hrms_lite/hrms_lite/settings.py
@@ -104,16 +104,16 @@ WSGI_APPLICATION = 'hrms_lite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    "default": dj_database_url.parse(os.environ["DATABASE_URL"])
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     "default": dj_database_url.parse(os.environ["DATABASE_URL"])
+# }
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
